a1144b59c1ee1aeb64e626779016697016d4dcb8
Jingjing Duan <duanjingjing@gmail.com>
2012-01-22 11:04:46 -0800

    delete invalid tests

--- spec/gem_analyzer_spec.rb
+++ spec/gem_analyzer_spec.rb
@@ -6,18 +6,4 @@ describe GemAnalyzer do
         #email.stub(:subject).and_return(subject)
         @analyzer = GemAnalyzer.new(@gem_downloader)
     end
-
-    context "#is_semver?" do
-        it "should return true for 10.1.3" do
-            @analyzer.is_semver?("10.1.3").should be_true
-        end
-
-        it "should return true for 1.2.3" do
-            @analyzer.is_semver?("1.2.3").should be_true
-        end
-
-        it "should return false for 1.2" do
-            @analyzer.is_semver?("1.2").should be_false
-        end
-    end
 end
